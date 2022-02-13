64974e852c15e0e2392233579db7f8ff0c0a30be
Michael Blydenburgh <mike.blydenburgh@gmail.com>
2022-02-13 11:11:13 -0500

    init commit, very basic http response.

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::{net::{TcpListener, TcpStream}, io::{Read, Write}};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream)
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    println!("Incoming request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
